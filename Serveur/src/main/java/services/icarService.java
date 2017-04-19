b8760366b519afb85474d6277ed269a14c6a507e
PONZO_Jb <jeanbaptiste.ponzo@gmail.com>
2017-04-19 17:50:32 +0200

    ajout req inscription and getcar

--- Serveur/src/main/java/services/icarService.java
+++ Serveur/src/main/java/services/icarService.java
@@ -1,11 +1,14 @@
 package services;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 import java.sql.*;
 
@@ -60,6 +63,52 @@ public class icarService {
 
     }
 
+    @RequestMapping(method = RequestMethod.POST, value ="/subscribe")
+    public String Subscribe(@RequestParam("UserLogin") String UserLogin,@RequestParam("UserMail") String UserMail ,@RequestParam("UserPassword") String UserPassword,@RequestParam("UserStatut") String UserStatut)
+    {
+
+        //Connection à la base de donnée avec la variable conn
+        Connection  conn = getConnection();
+
+        // On déclare les variables à utiliser
+        Statement statement;
+        PreparedStatement PrepStat;
+        ResultSet resultats;
+        String Req;
+        Req = "SELECT * FROM users WHERE login='"+UserLogin+ "' ;";
+
+        try {
+            statement =  conn.createStatement();
+            resultats = statement.executeQuery(Req);
+
+            if(resultats.next()) {
+
+
+                return "User Already exist";
+            }
+            else
+            {
+                Req = "INSERT INTO users ( mail, login, password, status) VALUES (?, ?, ?, ?)";
+                PrepStat = conn.prepareStatement(Req);
+
+                PrepStat.setString(1,UserMail);
+                PrepStat.setString(2,UserLogin);
+                PrepStat.setString(3,UserPassword);
+                PrepStat.setString(4,UserStatut);
+
+                int created = PrepStat.executeUpdate();
+                if(created ==1)
+                    return "utilisateur créé";
+                else
+                    return "erreur de création";
+            }
+        }
+        catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
 
     @RequestMapping(method = RequestMethod.POST, value ="/info")
     public Response getInfos(@RequestParam("UserLogin") String UserLogin)
@@ -90,6 +139,52 @@ public class icarService {
             {
                 return Response.status(400).build();
             }
+        }
+        catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value ="/userCars")
+    public Response getCars(@PathParam("UserLogin") String UserLogin)
+    {
+        JSONObject jsonCar = new JSONObject();
+        JSONArray CarArray = new JSONArray();
+        //Connection à la base de donnée avec la variable conn
+        Connection  conn = getConnection();
+
+        // On déclare les variables à utiliser
+        Statement statement;
+        ResultSet resultats;
+        String Req;
+        Req = "SELECT * FROM voiture WHERE id_proprietaire=(SELECT id FROM users WHERE login='"+UserLogin +"');";
+
+        try {
+            statement =  conn.createStatement();
+            resultats = statement.executeQuery(Req);
+
+            while(resultats.next()) {
+
+                try {
+                    jsonCar.put("id",resultats.getInt("id"));
+                    jsonCar.put("id_proprietaire",resultats.getInt("id_proprietaire"));
+                    jsonCar.put("Immatriculation",resultats.getString("Immatriculation"));
+                    jsonCar.put("Modèle",resultats.getInt("Modèle"));
+                    jsonCar.put("DateImmat",resultats.getDate("DateImmat"));
+                    jsonCar.put("CV",resultats.getInt("CV"));
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                CarArray.put(jsonCar);
+
+
+            }
+            return Response.ok(CarArray.toString()).build();
+
         }
         catch (SQLException e) {
             throw new RuntimeException(e);
